49f8ba571eb06b1de6de2b360a7a65783429c3bc
Cristian Arando <crisarandosyse@gmail.com>
2022-05-15 20:27:22 -0400

    vistas

--- app/src/main/java/com/example/simuladorplandenegocios/MainActivity.java
+++ app/src/main/java/com/example/simuladorplandenegocios/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.simuladorplandenegocios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button iniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iniciar=(Button)findViewById(R.id.iniciar);
+
+        iniciar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent( MainActivity.this, simulacion.class);
+                startActivity(i);
+            }
+        });
     }
 }
