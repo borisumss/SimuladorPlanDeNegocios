149740766ca30d65d1cb69b931fb525af9becc24
Boris Calli <201900823@est.umss.edu>
2022-05-29 00:41:43 -0400

    Presupuesto interfaz

---
+++ app/src/main/java/com/example/simuladorplandenegocios/Vista/PresupuestoDelEmprendimiento.java
@@ -0,0 +1,16 @@
+package com.example.simuladorplandenegocios.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.simuladorplandenegocios.R;
+
+public class PresupuestoDelEmprendimiento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presupuesto_del_emprendimiento);
+    }
+}
