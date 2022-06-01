9bfe83a70ed383e7c58ae23dae975e383063956c
Nailda Mollo <201904962@est.umss.edu>
2022-06-01 19:41:40 -0400

    Calculo de gastos

--- app/src/main/java/com/example/simuladorplandenegocios/Controlador/tab4.java
+++ app/src/main/java/com/example/simuladorplandenegocios/Controlador/tab4.java
@@ -2,20 +2,28 @@ package com.example.simuladorplandenegocios.Controlador;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.simuladorplandenegocios.R;
+
+import java.text.DecimalFormat;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link tab4#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class tab4 extends Fragment {
+public class tab4 extends Fragment{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +33,29 @@ public class tab4 extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    //Servicios
+    private EditText servicioLuz;
+    private EditText servicioAgua;
+    private EditText servicioTelefono;
+    private EditText servicioCelular;
+    //Mantenimientos
+    private EditText mantenimiento1;
+    private EditText mantenimiento2;
+    private EditText mantenimiento3;
+    private EditText mantenimiento4;
+    //Otros
+    private EditText salud;
+    private EditText imprevistos;
+    private TextView textoSalidaServicios;
+    private TextView textoSalidaMantenimiento;
+    private TextView textoSalidaGastoTotal;
+    private double totalServicio;
+    private double totalMantenimiento;
+    private double totalGastos;
+    //Boton Calculo valor servicio
+    private Button calcular;
+    private EditText impuestos;
+    private EditText alimentacion;
     public tab4() {
         // Required empty public constructor
     }
@@ -57,10 +87,78 @@ public class tab4 extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab4, container, false);
+        View s = inflater.inflate(R.layout.fragment_tab4, container, false);
+        calcular=(Button)s.findViewById(R.id.button2);
+
+        //Servicios
+        servicioLuz=(EditText) s.findViewById(R.id.sInput);
+        servicioAgua=(EditText) s.findViewById(R.id.servicioAguaInput);
+        servicioTelefono=(EditText) s.findViewById(R.id.ServicioTelefono);
+        servicioCelular=(EditText) s.findViewById(R.id.servicioCelularInput);
+        //totales
+        textoSalidaServicios=(TextView) s.findViewById(R.id.totservices);
+        //Mantenimiento
+        mantenimiento1=(EditText) s.findViewById(R.id.mm1Input);
+        mantenimiento2=(EditText) s.findViewById(R.id.mm2Input);
+        mantenimiento3=(EditText) s.findViewById(R.id.m3input);
+        mantenimiento4=(EditText) s.findViewById(R.id.mm4put);
+        //totales
+        textoSalidaMantenimiento=(TextView) s.findViewById(R.id.textView32);
+       //gastos aparte
+        salud=(EditText)s.findViewById(R.id.eput);
+        imprevistos=(EditText) s.findViewById((R.id.imprevistosinput));
+        //Gastos iniciales
+        impuestos=(EditText)s.findViewById(R.id.impuestoInput);
+        alimentacion=(EditText)s.findViewById(R.id.alimentacionInput);
+        //totales
+        textoSalidaMantenimiento=(TextView) s.findViewById(R.id.textView32);
+        textoSalidaGastoTotal = (TextView) s.findViewById(R.id.textView328);
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcularTotalServicio();
+                calcularTotalMantenimiento();
+                calcularTotalGastos();
+            }
+        });
+     return s;
+    }
+
+   public void calcularTotalServicio(){
+       DecimalFormat formato = new DecimalFormat("#.00");
+       EditText[]arra = {servicioCelular,servicioLuz,servicioAgua,servicioTelefono};
+       totalServicio = 0;
+       for(EditText i : arra){
+           if (!i.getText().toString().isEmpty())
+               totalServicio = totalServicio+Double.parseDouble(i.getText().toString());
+       }
+       textoSalidaServicios.setText("Total en servicios es :  "+ formato.format(totalServicio)+"Bs");
+    }
+
+    public void calcularTotalMantenimiento(){
+        DecimalFormat formato = new DecimalFormat("#.00");
+        EditText[]arra = {mantenimiento1,mantenimiento2,mantenimiento3,mantenimiento4};
+        totalMantenimiento = 0;
+        for(EditText i : arra){
+            if (!i.getText().toString().isEmpty())
+                totalMantenimiento = totalMantenimiento+Double.parseDouble(i.getText().toString());
+        }
+        textoSalidaMantenimiento.setText("Total en mantenimiento es : "+ formato.format(totalMantenimiento)+"Bs");
+    }
+
+    public void calcularTotalGastos(){
+        DecimalFormat formato = new DecimalFormat("#.00");
+        EditText[]arra = {mantenimiento1,mantenimiento2,mantenimiento3,mantenimiento4,impuestos,alimentacion,servicioTelefono,servicioLuz,servicioAgua,servicioCelular,salud};
+        totalGastos = 0;
+        for(EditText i : arra){
+            if (!i.getText().toString().isEmpty())
+                totalGastos = totalGastos+Double.parseDouble(i.getText().toString());
+        }
+        textoSalidaGastoTotal.setText("Total en gastos es : "+ formato.format(totalGastos)+"Bs");
     }
 }
