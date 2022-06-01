572d8ecdbff082a70c346dd0403fb2cb92dcd400
Cristian Arando <crisarandosyse@gmail.com>
2022-06-01 02:14:50 -0400

    solo falta de totales

--- app/src/main/java/com/example/simuladorplandenegocios/Controlador/tab2.java
+++ app/src/main/java/com/example/simuladorplandenegocios/Controlador/tab2.java
@@ -50,6 +50,13 @@ public class tab2 extends Fragment {
     private TextView montoSolicitado;
     private TextView montoFinanciar;
     private TextView condicionMonto;
+    Double totalProy;//totalAP+totalR-efectivo
+    //totalAP es solo el aporte total
+    //el porcentaje de aporte propio solo es totalAP/totalproyecto ojo con 0/0
+    //condicion AP si porcentaje es menor a 0.1 "no cumple con aporte propio minimo del 10%"; si cumple con aporte propio
+    //monto solicitado(de otra tabla)
+    //monto a financiar = totalReq - efectivo
+    //condicion monto a financiar correcto ->    si monto solicitado = monto a financiar: "monto a fnianciar correcto"; monto a financiar distinto al solicitado
 
     public tab2() {
         // Required empty public constructor
@@ -87,16 +94,71 @@ public class tab2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_tab2, container, false);
+
         calcular = (Button) v.findViewById(R.id.calcular);
+
+        //Aporte propio
+        efectivoAP = (EditText) v.findViewById(R.id.numEfectivoAP);
+        manoObraAP = (EditText) v.findViewById(R.id.numManoAP);
+        materiaPrimaAP = (EditText) v.findViewById(R.id.numMateriaAP);
+        promocionAP = (EditText) v.findViewById(R.id.numReqAP);
+        infraestructuraAP = (EditText) v.findViewById(R.id.numInfraAP);
+        maquinariaAP = (EditText) v.findViewById(R.id.numMaquiAP);
+        legalAP = (EditText) v.findViewById(R.id.numLegalAP);
+        //total ap
+        totalA = (TextView) v.findViewById(R.id.totalAP);
+        //Requerimiento
+        gastosR = (EditText) v.findViewById(R.id.gastosR);
+        materiaPrimaR = (EditText) v.findViewById(R.id.numMateriaR);
+        promocionR = (EditText) v.findViewById(R.id.numReqR);
+        infraestructuraR = (EditText) v.findViewById(R.id.numInfraR);
+        maquinariaR = (EditText) v.findViewById(R.id.numMaquiR);
+        legalR = (EditText) v.findViewById(R.id.numLegalR);
+        //total R
+        totalReq = (TextView) v.findViewById(R.id.totalR);
+        //totales
+        totalProyecto = (TextView) v.findViewById(R.id.totalProyecto);
+        totalAportePropio = (TextView) v.findViewById(R.id.aportePropio);
+        porcentajeAportePropio = (TextView) v.findViewById(R.id.aportePropioPorc);
+        condicionAP = (TextView) v.findViewById(R.id.condicionAP);
+        montoSolicitado = (TextView) v.findViewById(R.id.montoSolicitado);
+        montoFinanciar = (TextView) v.findViewById(R.id.montoFinanciar);
+        condicionMonto = (TextView) v.findViewById(R.id.condicionMonto);
+
         calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                condicionMonto = (TextView)v.findViewById(R.id.condicionMonto);
-                condicionMonto.setText("holaaaa");
+                calcularTotalAP();
+                calcularTotalR();
+                calcularTotales();
             }
         });
 
         return v;
+    }
+
+    private void calcularTotalAP() {
+        EditText[] array = {efectivoAP, manoObraAP, materiaPrimaAP, promocionAP, infraestructuraAP, maquinariaAP, legalAP};
+        totalAP = 0;
+        for (EditText i : array) {
+            if (!i.getText().toString().isEmpty())
+                totalAP += Double.parseDouble(i.getText().toString());
+        }
+        totalA.setText("TOTAL Aporte Propio " + totalAP + " Bs");
+    }
+
+    private void calcularTotalR() {
+        EditText[] array = {gastosR, materiaPrimaR, promocionR, infraestructuraR, maquinariaR, legalR};
+        totalR = 0;
+        for (EditText i : array) {
+            if (!i.getText().toString().isEmpty())
+                totalAP += Double.parseDouble(i.getText().toString());
+        }
+        totalReq.setText("TOTAL Requerimiento " + totalAP + " Bs");
+    }
+
+    private void calcularTotales() {
+
     }
 
 }
