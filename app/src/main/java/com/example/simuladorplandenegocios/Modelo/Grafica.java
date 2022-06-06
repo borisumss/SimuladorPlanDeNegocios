074c6a178ec338aa4df8babc63d80ec939f050bf
Nailda Mollo <201904962@est.umss.edu>
2022-06-06 15:17:17 -0400

    Paso de nombre a grafica

--- app/src/main/java/com/example/simuladorplandenegocios/Modelo/Grafica.java
+++ app/src/main/java/com/example/simuladorplandenegocios/Modelo/Grafica.java
@@ -2,11 +2,15 @@ package com.example.simuladorplandenegocios.Modelo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.simuladorplandenegocios.R;
 import com.github.mikephil.charting.charts.LineChart;
@@ -24,10 +28,7 @@ import java.util.ArrayList;
  */
 public class Grafica extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+   private TextView tv1;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -37,31 +38,23 @@ public class Grafica extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Grafica.
-     */
     // TODO: Rename and change types and number of parameters
     public static Grafica newInstance(String param1, String param2) {
         Grafica fragment = new Grafica();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        getParentFragmentManager().setFragmentResultListener("nombre", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String resultado = result.getString("NombreProyecto");
+                tv1.setText(resultado);
+            }
+
+        });
     }
 
     @Override
@@ -89,6 +82,11 @@ public class Grafica extends Fragment {
         return dataVals;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tv1= view.findViewById(R.id.SimulacionR);
+    }
 }
 
 
