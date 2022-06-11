bc02cadb81bc4266a58825e31dae0b6eb38a1856
Jheison Leon <201905980@est.umss.edu>
2022-06-11 11:35:20 -0400

    otro metodo para descargar

--- app/src/main/java/com/example/simuladorplandenegocios/Vista/MainActivity.java
+++ app/src/main/java/com/example/simuladorplandenegocios/Vista/MainActivity.java
@@ -17,11 +17,14 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.example.simuladorplandenegocios.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -31,6 +34,7 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
     private Button iniciar;
     private Button simulacion,ayuda;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main
         );
 
-
+        //seekBar = (SeekBar) findViewById(R.id.seekbar);
         iniciar=(Button)findViewById(R.id.formularioButton);
         simulacion=(Button)findViewById(R.id.simulacionButton);
         ayuda =(Button)findViewById(R.id.ayudaButton);
@@ -62,33 +66,46 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-               /* File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Simulacion", "Manual.pdf");
-                if(!file.exists()){
-
-                }else{
-                    file.delete();
-                }*/
-
-              FirebaseStorage fb = FirebaseStorage.getInstance();
-                StorageReference ref = fb.getReference().child("Proyecto Final RA 01-2022.pdf");
-                ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        String url = uri.toString();
-                        descargar(MainActivity.this,"Manual.pdf",DIRECTORY_DOWNLOADS,url);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
-                    }
-                });
+               //descarga automatica
+                Uri uri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/simulador-plan-de-negocios.appspot.com/o/Proyecto%20Final%20RA%2001-2022.pdf?alt=media&token=284b0aca-c1bc-4389-b15b-22fe4e6acb14");
+                Intent i = new Intent( Intent.ACTION_VIEW, uri);
+                startActivity(i);
             }
         });
 
 
     }
 
+    public void descargarManual(){
+         File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Simulacion", "Manual-de-Usuario.pdf");
+                if(!file.exists()){
+                    FirebaseStorage fb = FirebaseStorage.getInstance();
+                    StorageReference ref = fb.getReference().child("Proyecto Final RA 01-2022.pdf");
+                    ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String url = uri.toString();
+                            descargar(MainActivity.this,"Manual-de-Usuario.pdf",DIRECTORY_DOWNLOADS,url);
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                          /* try {
+                                sleep(4000);
+                                Intent i = new Intent( MainActivity.this, VistaManual.class);
+                                System.out.println("Comienza el sueño");
+                                startActivity(i);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }*/
+                        }
+                    }) ;
+                }else{
+                    //Intent i = new Intent( MainActivity.this, VistaManual.class);
+                   //startActivity(i);
+                    file.delete();
+                }
+    }
 
     public void salir(View view){
         finish();
@@ -106,18 +123,6 @@ public class MainActivity extends AppCompatActivity {
         request.setDestinationInExternalFilesDir(context,destino,nombreArchivo);
         request.setDestinationInExternalPublicDir("/Simulacion",nombreArchivo);
         downloadManager.enqueue(request);
-
-        /*File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Simulacion", "Manual.pdf");
-        boolean aux = true;
-        while(aux == true){
-            if(file.exists()){
-
-                aux = false;
-            }else{
-                System.out.println("Esperando...");
-                Toast.makeText(MainActivity.this, "Descargando manual", Toast.LENGTH_LONG).show();
-            }
-        }*/
 
     }
 
